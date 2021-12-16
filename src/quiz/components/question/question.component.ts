d1fe787f9d42f000f978ec848716a3806f4b926b
Fadi Serapian <fadi.serapian@gmail.com>
2021-12-16 11:29:43 -0500

    add question text to question comp html

--- src/quiz/components/question/question.component.ts
+++ src/quiz/components/question/question.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { QuizService } from 'src/quiz/services/quiz.service';
+import { Question } from 'src/quiz/types/question.interface';
+import { QuizState } from 'src/quiz/types/quiz-state.interface';
 
 @Component({
   selector: 'app-quiz-question',
@@ -7,4 +11,11 @@ import { Component } from '@angular/core';
 })
 export class QuestionComponent {
   title = 'question component';
+  question$: Observable<Question>;
+
+  constructor(private quizService: QuizService) {
+    this.question$ = this.quizService.state$.pipe(
+      map((state: QuizState) => state.questions[state.currentQuestionIndex])
+    );
+  }
 }
